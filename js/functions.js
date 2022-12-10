d6dfb26c81d557c4380236080f2b20fd0acd5231
William <lavit.william@gmail.com>
2022-12-10 18:38:47 +0100

    import File function done !

--- js/functions.js
+++ js/functions.js
@@ -39,6 +39,31 @@ function fileManager(data) {
         $('#_virtualPath_').append(`<label style="white-space: nowrap;">${path}</label>`)
     }
 
+    const addNewFile = (name) => {
+        $('#importNewFile').off('change')
+        $('#importNewFile')[0].click()
+        $('#importNewFile').on('change', (e) => {
+            const files = e.target.files
+            let reader = new FileReader()
+            reader.onload = (file) => {
+                itemId++
+                const item = getFolderById(currentPath)
+                item.items.push({
+                    name: `${name}(${itemId})`,
+                    type: "FILE",
+                    id: itemId,
+                    active: false,
+                    renameElement: false,
+                    renameElementStructure: false,
+                    data: reader.result
+                })
+                updateFilesItemsStructure()
+                updateFilesItems(getFolderById(currentPath))
+            }
+            reader.readAsDataURL(files[0])
+        })
+    }
+
     const addNewFolder = (name) => {
         itemId++
         const item = getFolderById(currentPath)
@@ -60,7 +85,9 @@ function fileManager(data) {
         const recursive = (items) => {
             for (let i = 0; i < items.length; i++) {
                 items[i] = { ...items[i], ...obj }
-                recursive(items[i].items)
+                if (items[i].type === "FOLDER") {
+                    recursive(items[i].items)
+                }
             }
         }
         recursive(items.items)
@@ -120,7 +147,7 @@ function fileManager(data) {
         $('._labelRightClickSelection_').on('click', (e) => {
             switch (+e.target.id.split('_').at(-1)) {
                 case 0:
-                    if (target.attr('class') === "_fileItemStructureElement_") {
+                    if (target.attr('class') === "_fileItemStructureElement_" || target.attr('class')?.split(' ')[1] === "_fileElementStructure_") {
                         renameItemByElement(target, "elementStructure")
                     }
                     else {
@@ -144,7 +171,8 @@ function fileManager(data) {
         const cardWidth = 70
         $(`#_filesItems`).children().remove()
         for (let i = 0; i < item.items.length; i++) {
-            $(`#_filesItems`).append(`<div style="height: min-content; padding-bottom: 5px;">
+            if (item.items[i].type === "FOLDER") {
+                $(`#_filesItems`).append(`<div style="height: min-content; padding-bottom: 5px;">
                 <div style="width: ${cardWidth}px; height: ${cardWidth}px; margin: 10px 15px;" class="_filesItemsElementSelected_" id="_filesItemsElementSelected_${item.items[i].id}">
                     <div style="width: ${cardWidth - 10}px; height: ${cardWidth - 10}px; margin-left: 5px; margin-top: 5px; pointer-events: none;">
                         <svg style="fill: ${textColor}; width: ${cardWidth - 10}px; pointer-events: none;" viewBox="0 0 512 512"><path style="pointer-events: none;" d="M447.1 96h-172.1L226.7 50.75C214.7 38.74 198.5 32 181.5 32H63.1c-35.35 0-64 28.66-64 64v320c0 35.34 28.65 64 64 64h384c35.35 0 64-28.66 64-64V160C511.1 124.7 483.3 96 447.1 96zM463.1 416c0 8.824-7.178 16-16 16h-384c-8.822 0-16-7.176-16-16V96c0-8.824 7.178-16 16-16h117.5c4.273 0 8.293 1.664 11.31 4.688L255.1 144h192c8.822 0 16 7.176 16 16V416z"/></svg>
@@ -152,6 +180,17 @@ function fileManager(data) {
                     ${item.items[i].renameElement ? `<input id="_renameTextElem_" type="text" value="${item.items[i].name}" style="width: 100%;"/>` : `<label style="font-size: ${fileCardTextHeight}px;">${item.items[i].name.length > fileCardMaxTextLength ? `${item.items[i].name.substring(0, fileCardMaxTextLength)}...` : item.items[i].name}</label>`}
                 </div>
             </div>`)
+            }
+            else if (item.items[i].type === "FILE") {
+                $(`#_filesItems`).append(`<div style="height: min-content; padding-bottom: 5px;">
+                <div style="width: ${cardWidth}px; height: ${cardWidth}px; margin: 10px 15px;" class="_filesItemsElementSelected_" id="_filesItemsElementSelected_${item.items[i].id}">
+                    <div style="width: ${cardWidth - 10}px; height: ${cardWidth - 10}px; margin-left: 5px; margin-top: 5px; pointer-events: none;">
+                    <svg style="fill: ${textColor}; width: ${cardWidth - 25}px; pointer-events: none;" viewBox="0 0 384 512"><path style="pointer-events: none;" d="M0 64C0 28.65 28.65 0 64 0H229.5C246.5 0 262.7 6.743 274.7 18.75L365.3 109.3C377.3 121.3 384 137.5 384 154.5V448C384 483.3 355.3 512 320 512H64C28.65 512 0 483.3 0 448V64zM336 448V160H256C238.3 160 224 145.7 224 128V48H64C55.16 48 48 55.16 48 64V448C48 456.8 55.16 464 64 464H320C328.8 464 336 456.8 336 448z"/></svg>
+                    </div>
+                    ${item.items[i].renameElement ? `<input id="_renameTextElem_" type="text" value="${item.items[i].name}" style="width: 100%;"/>` : `<label style="font-size: ${fileCardTextHeight}px;">${item.items[i].name.length > fileCardMaxTextLength ? `${item.items[i].name.substring(0, fileCardMaxTextLength)}...` : item.items[i].name}</label>`}
+                </div>
+            </div>`)
+            }
         }
         $('#_renameTextElem_')[0]?.select()
         $(`._filesItemsElementSelected_`).on('mousedown', (e) => {
@@ -194,10 +233,10 @@ function fileManager(data) {
     const editFolderById = (id, obj) => {
         const recursive = (items) => {
             for (let i = 0; i < items.length; i++) {
-                if (items[i].type === "FOLDER" && items[i].id === id) {
+                if (items[i].id === id) {
                     items[i] = { ...items[i], ...obj }
                 }
-                else {
+                else if (items[i].type === "FOLDER") {
                     recursive(items[i].items)
                 }
             }
@@ -216,10 +255,10 @@ function fileManager(data) {
         if (_index === -1) {
             const recursive = (items) => {
                 for (let i = 0; i < items.length; i++) {
-                    if (items[i].type === "FOLDER" && items[i].id === id) {
+                    if (items[i].id === id) {
                         items.splice(i, 1)
                     }
-                    else {
+                    else if (items[i].type === "FOLDER") {
                         recursive(items[i].items)
                     }
                 }
@@ -308,6 +347,7 @@ function fileManager(data) {
                     <svg style="fill: ${textColor}; width: ${buttonHeight}px; pointer-events: none;" viewBox="0 0 512 512"><path style="pointer-events: none;" d="M447.1 96h-172.1L226.7 50.75C214.7 38.74 198.5 32 181.5 32H63.1c-35.35 0-64 28.66-64 64v320c0 35.34 28.65 64 64 64h384c35.35 0 64-28.66 64-64V160C511.1 124.7 483.3 96 447.1 96zM463.1 416c0 8.824-7.178 16-16 16h-384c-8.822 0-16-7.176-16-16V96c0-8.824 7.178-16 16-16h117.5c4.273 0 8.293 1.664 11.31 4.688L255.1 144h192c8.822 0 16 7.176 16 16V416z"/></svg>
                 </div>
                 <div class="${fileManagerName}" id="__importFile" style="width: ${buttonHeight}px; height: ${buttonHeight}px; border-color: ${textColor}; border: solid ${textColor} 2px; border-radius: 5px; padding: 5px; margin: 0px ${marginTopElements}px; text-align: center;">
+                    <input type="file" id="importNewFile" style="display: none;" />
                     <svg style="fill: ${textColor}; height: ${buttonHeight}px; width: ${buttonHeight}px; pointer-events: none;" viewBox="0 0 384 512"><path style="pointer-events: none;" d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 232V334.1l31-31c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-72 72c-9.4 9.4-24.6 9.4-33.9 0l-72-72c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l31 31V232c0-13.3 10.7-24 24-24s24 10.7 24 24z"/></svg>
                 </div>
                 <div class="${fileManagerName}" id="__goBack" style="width: ${buttonHeight}px; height: ${buttonHeight}px; border-color: ${textColor}; border: solid ${textColor} 2px; border-radius: 5px; padding: 5px; margin: 0px ${marginTopElements}px; text-align: center;">
@@ -351,6 +391,16 @@ function fileManager(data) {
                 </div>
             </div>`)
         }
+        const addNewFile = (id, active, name, renameElementStructure) => {
+            $(`#_filesItemsStructure`).append(`<div id="_fileItemStructureElement_${id}" class="_fileItemStructureElement_ _fileElementStructure_" style="height: ${cardHeight}px; margin-left: calc((25px + ${(margin * decal)}px) - ${decal}px); margin-bottom: 5px; width: auto; padding: 0; width: max-content; ${currentPath === id ? `background-color:${cardBackGroundColorOnClick}` : ""}">
+                <div style="display: flex; pointer-events: none;">
+                    <div class="_activeCardChildren ${active ? "active" : "no-active"}" id="_FileStructure_${id}" style="padding: 0px; margin-left: 5px; pointer-events: auto; background-color:transparent; margin-top: -1px;">
+                    <svg style="fill: ${textColor}; width: 12px; pointer-events: none;" viewBox="0 0 384 512"><path style="pointer-events: none;" d="M0 64C0 28.65 28.65 0 64 0H229.5C246.5 0 262.7 6.743 274.7 18.75L365.3 109.3C377.3 121.3 384 137.5 384 154.5V448C384 483.3 355.3 512 320 512H64C28.65 512 0 483.3 0 448V64zM336 448V160H256C238.3 160 224 145.7 224 128V48H64C55.16 48 48 55.16 48 64V448C48 456.8 55.16 464 64 464H320C328.8 464 336 456.8 336 448z"/></svg>
+                    </div>
+                    ${renameElementStructure ? `<input id="_renameTextElem_" type="text" value="${name}" style="width: 100%;"/>` : `<label style="margin-left: 10px; pointer-events: none; white-space: nowrap; margin-right: 10px; font-size: ${fileCardTextHeight}px;">${name.length > fileCardMaxTextLength ? `${name.substring(0, fileCardMaxTextLength)}...` : name}</label>`}
+                </div>
+            </div>`)
+        }
         const recursive = (items) => {
             margin++
             for (let i = 0; i < items.length; i++) {
@@ -362,7 +412,7 @@ function fileManager(data) {
                         }
                         break
                     case "FILE":
-                    // Do some stuff ...
+                        addNewFile(items[i].id, items[i].active, items[i].name, items[i].renameElementStructure)
                 }
             }
             margin--
@@ -418,16 +468,16 @@ function fileManager(data) {
             if ($(e.target).attr('class').split(" ")[0] !== "_activeCardChildren") {
                 switch (e.which) {
                     case 1:
-                        if ($(e.target).attr('class') !== "_labelRightClickSelection_") {
+                        if ($(e.target).attr('class') !== "_labelRightClickSelection_" && $(e.target).attr('class').split(' ')[1] !== "_fileElementStructure_") {
                             currentPath = _idFolder
                             updateVirtualPath(getPathName(currentPath))
                             setAllDefaultParameter({ renameElement: false, renameElementStructure: false })
                             currentPath = currentPath
                             updateFilesItemsStructure()
-                            $('._fileItemStructureElement_').css('backgroundColor', '')
-                            $(`#${e.target.id}`).css('backgroundColor', cardBackGroundColorOnClick)
                             updateFilesItems(getFolderById(currentPath))
                         }
+                        $('._fileItemStructureElement_').css('backgroundColor', '')
+                        $(`#${e.target.id}`).css('backgroundColor', cardBackGroundColorOnClick)
                         break
                     case 3:
                         if ($(e.target).attr('class') !== "_labelRightClickSelection_" && +$(e.target).attr('id').split('_')[2] !== 0) {
@@ -444,7 +494,7 @@ function fileManager(data) {
                 }
             }
         })
-        if (typeof (div) !== "undefined" && div.attr('class') == "_fileItemStructureElement_") {
+        if (typeof (div) !== "undefined" && (div.attr('class') == "_fileItemStructureElement_" || div.attr('class')?.split(' ')[1] === "_fileElementStructure_")) {
             $('#_renameTextElem_').off('keypress')
             $('#_renameTextElem_').on('keypress', (e) => {
                 if (e.keyCode === 13) {
@@ -473,13 +523,14 @@ function fileManager(data) {
             case "__addFolder":
                 if (typeof (data.onAddFolderFocus) !== "undefined") {
                     data.onAddFolderFocus()
-                    addNewFolder("folder")
                 }
+                addNewFolder("folder")
                 break
             case "__importFile":
                 if (typeof (data.onImportFileFocus) !== "undefined") {
                     data.onImportFileFocus()
                 }
+                addNewFile("file")
                 break
             case "__goBack":
                 if (typeof (data.onGoBackFocus) !== "undefined") {
@@ -494,7 +545,7 @@ function fileManager(data) {
         }
     })
     $('#_FileManager_').on('mousedown', (e) => {
-        if ($(e.target).attr('class') !== "_labelRightClickSelection_" && $(e.target).attr('class') !== "_filesItemsElementSelected_" && $(e.target).parent().attr('class') !== "_filesItemsElementSelected_" && $(e.target).attr('class') !== "_fileItemStructureElement_" && $(e.target).attr('class') !== "__Item__FileManager__") {
+        if ($(e.target).attr('class') !== "_labelRightClickSelection_" && $(e.target).attr('class') !== "_filesItemsElementSelected_" && $(e.target).parent().attr('class') !== "_filesItemsElementSelected_" && $(e.target).attr('class') !== "_fileItemStructureElement_" && $(e.target).attr('class') !== "__Item__FileManager__" && $(e.target).attr('class')?.split(' ')[1] !== "_fileElementStructure_") {
             setAllDefaultParameter({ renameElement: false, renameElementStructure: false })
             updateFilesItems(getFolderById(currentPath))
             updateFilesItemsStructure()
